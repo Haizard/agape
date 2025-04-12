4c69402eed7f740c614b5a06aaa43f6dd8eb8efc
Haizard <haithammsiape@gmail.com>
2025-04-12 05:19:51 -0700

    Simplify Netlify Functions

--- frontend/school-frontend-app/netlify/functions/options.js
+++ frontend/school-frontend-app/netlify/functions/options.js
@@ -1,12 +1,10 @@
 exports.handler = async function(event, context) {
   // This function handles OPTIONS requests for CORS preflight
-  console.log('Handling OPTIONS request for CORS preflight');
-  
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Max-Age': '86400'
     },
